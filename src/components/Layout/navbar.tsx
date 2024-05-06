56ba5b33faf5514a01a83ea38d4216b118bb271f
WilliamLourensius <67148489+WilliamLourensius@users.noreply.github.com>
2024-05-06 10:29:53 +0700

    update finish

--- src/components/Layout/navbar.tsx
+++ src/components/Layout/navbar.tsx
@@ -2,33 +2,31 @@
 import logo from '@/images/logo.png'
 
 const Navbar = () => {
-  const [isActive, setIsActive] = useState('');
-  const scrollToElement = (elementId: string) => {
-    const element = document.getElementById(elementId);
-    setIsActive(elementId)
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
+  // const [isActive, setIsActive] = useState('');
+  // const scrollToElement = (elementId: string) => {
+  //   const element = document.getElementById(elementId);
+  //   setIsActive(elementId)
+  //   if (element) {
+  //     element.scrollIntoView({ behavior: 'smooth' });
+  //   }
+  // };
 
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <div className={`navbar ${isMenuOpen ? 'ht-100-max' : ''}`}>
-      <Link to="/">
-        <div onClick={() => {
-          scrollToElement('myprofile');
-        }} className='flex ml-10'>
+      {/* <Link to="/"> */}
+        <div className='flex ml-10'>
           <img src={logo} alt="" />
           <p>Willrens</p>
         </div>
-      </Link>
+      {/* </Link> */}
       <div className='menu-toggle' onClick={() => setIsMenuOpen(!isMenuOpen)}>
         <div className="bar1" />
         <div className="bar mt-5" />
         <div className="bar mt-5" />
       </div>
-      <ul className={`menu ${isMenuOpen ? 'open animation-slide' : ''}`}>
+      {/* <ul className={`menu ${isMenuOpen ? 'open animation-slide' : ''}`}>
         <li className={`menu isClickAble animation-child 
           ${isMenuOpen ? 'open animation-slide' : ''}  
           ${isActive === 'about' ? 'txt-secondaryS' : 'txt-default'} `}
@@ -61,12 +59,12 @@ const Navbar = () => {
           }}>
           Contacts
         </li>
-      </ul>
-      <div className={`menu mr-10 ${isMenuOpen ? 'open animation-slide ptb-10' : ''}`}>
+      </ul> */}
+      {/* <div className={`menu mr-10 ${isMenuOpen ? 'open animation-slide ptb-10' : ''}`}>
         <Link to="https://blog.willrens.com">
           <button className='primary-filled-btn plr-40 animation-child'>My Blog</button>
         </Link>
-      </div>
+      </div> */}
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,75 +1,129 @@
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import Layout from "../components/layout/layout";
-import Foto from '@/images/FotoProfile.png'
+import axios from 'axios';
 
-import logo from '@/images/logo.png'
+interface Player {
+  id: string;
+  name: string;
+  age: number;
+  birthPlace: string;
+}
+
 const Home = () => {
-  const fileId = '1ci_2jRGNzjx8olZ-q2Hs2QAL0fFBWQpE';
-  const fileUrl = `https://drive.google.com/uc?export=download&id=${fileId}`;
-  const scrollToElement = (elementId: string) => {
-    const element = document.getElementById(elementId);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+  const [data, setData] = useState<Player[]>([]);
+  const [playerData, setPlayerData] = useState<Player | null>(null);
+  const [selectedOption, setSelectedOption] = useState('');
+  const [showOptions, setShowOptions] = useState<boolean>(false);
+  const [newPlayer, setNewPlayer] = useState({
+    name: '',
+    age: 0,
+    birthPlace: ''
+  });
+
+  const handleChange = (event: string) => {
+    setSelectedOption(event);
+    setShowOptions(false);
+    setPlayerData(null);
+    fetchData(event);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async (birthPlace?: string) => {
+    try {
+      let url = 'https://localhost:7004/api/Player';
+      if (birthPlace) {
+        url += `?birthPlace=${encodeURIComponent(birthPlace)}`;
+      }
+      const response = await axios.get(url);
+      setData(response.data.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
     }
   };
+
+  const handleRequest = async (itemId: string) => {
+    try {
+      const response = await axios.get('https://localhost:7004/api/Player/' + itemId);
+      setPlayerData(response.data);
+    } catch (error) {
+      console.error('Error fetching player data:', error);
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setNewPlayer(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const postData = async () => {
+    try {
+      // Send POST request with new player data
+      await axios.post('https://localhost:7004/api/Player', newPlayer);
+
+      // Fetch updated data
+      fetchData();
+
+      // Reset input fields
+      setNewPlayer({        
+        name: '',
+        age: 0,
+        birthPlace: ''
+      });
+    } catch (error) {
+      console.error('Error posting data:', error);
+    }
+  };
+
   return (
     <Layout>
       <section>
         <div id='myprofile' className='background-section-first'>
-          <div className='container-layout fullCenter-flex'>
+          <div className='container-layout fullCenter-flex flex-column'>
             <div className='fullCenter-flex isWrap home-layout'>
-              <div className="wd-800-max">
-                <img className="wd-600-max obj-fit-cover" src={Foto} alt="" />
-              </div>
-              <div className="txt-left mr-500">
-                <p className="isBold fz-40 txt-default mt-20">I'm William</p>
-                <p className="fw-500 fz-40 txt-secondaryS mt-20 typewriter"> A Software Developer</p>
-                <button className='primary-outlined-btn plr-40 mt-20 isBold' onClick={() => {
-                  scrollToElement('about');
-                }}>About Me</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section id='about'>
-        <div className='background-section-second'>
-          <div className='container-layout-50 fullCenter-flex'>
-            <div className='fullCenter-flex resume wd-1000-max rd-10 '>
-              <div className='p-20 columncenter-flex rd-ltlb-10 ht-300 wd-50-cent'>
-                <img className='square-300-max p-20' src={logo} alt="" />
-              </div>
-              <div className='bg-gray-bg p-20 rd-rtrb-10 txt-secondaryS fw-500 txt-left center-flexColumn wd-600-max ht-400'>
-                <p className='isBold fz-40 mb-10'>About Me</p>
-                Hello my name is William and I'm a dedicated freelance web developer at Solid Team, harnessing my skills in both front-end and backend engineering to the fullest. It's my pride to graduate from Atma Jaya Yogyakarta, where I not only excelled academically but also dived into several challenging freelance projects. Additionally, I've served as a coordinator for national events at UAJY and acted as an ambassador, showcasing our university's achievements to various high schools.
-                <br></br>
-                These experiences have enriched me with a solid foundation in both technical aspects of web development and soft skills. I'm not just proficient in coding but also possess qualities like perseverance and patience in making crucial decisions. I remain deeply enthusiastic about continually learning and advancing in my role.
-                <Link to={fileUrl}>
-                  <button className='secondary-filled-btn plr-20 animation-child mt-20'>My Resume</button>
-                </Link>
+              <div className="wd-800-max txt-left">
+                <div className="dropdown mb-10">
+                  <div className='mb-10'>
+                    <h2>Tambah Pemain Baru</h2>
+                    <input type="text" name="name" value={newPlayer.name} onChange={handleInputChange} placeholder="Name" />
+                    <input type="number" name="age" value={newPlayer.age} onChange={handleInputChange} placeholder="Age" />
+                    <input type="text" name="birthPlace" value={newPlayer.birthPlace} onChange={handleInputChange} placeholder="Birth Place" />
+                    <button onClick={postData}>Tambah</button>
+                  </div>
+                  <div className="select" onClick={() => setShowOptions(!showOptions)}>
+                    {selectedOption || 'Semua'}
+                  </div>
+                  <div className={`option-list ${showOptions ? 'show' : ''}`}>
+                    <div className="option" onClick={() => handleChange('')}>Semua</div>
+                    <div className="option" onClick={() => handleChange('Europe')}>Europe</div>
+                    <div className="option" onClick={() => handleChange("South America")}>South America</div>
+                  </div>
+                </div>
+                {data.map((item, index) => (
+                  <div className="card isClickAble" key={index} onClick={() => handleRequest(item.id)}>
+                    <div className="card-body">
+                      <h5 className="card-title">{item.name}</h5>
+                    </div>
+                  </div>
+                ))}
               </div>
             </div>
-          </div>
-        </div>
-      </section>
-      <section id='skill'>
-        <div className='background-section-third'>
-          <div className='container-layout'>
-
-          </div>
-        </div>
-      </section>
-      <section id='project'>
-        <div className='background-section-fourth'>
-          <div className='container-layout'>
-
-          </div>
-        </div>
-      </section>
-      <section id='contact'>
-        <div className='background-section-fifth'>
-          <div className='container-layout-50'>
-
+            <div className='flex-column'>
+              <h1>Data Pemain</h1>            
+              {playerData ? (
+                <>
+                  <p className="card-text">Age: {playerData.age}</p>
+                  <p className="card-text">Birth Place: {playerData.birthPlace}</p>
+                </>
+              ) : (
+                <p>No player data selected</p>
+              )}
+            </div>
           </div>
         </div>
       </section>
